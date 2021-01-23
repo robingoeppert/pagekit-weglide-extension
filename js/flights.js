7b047dbde1405338dad9deee50e0b0dc2c2a2bc1
robingoeppert <robin.goeppert@gmail.com>
2021-01-23 20:57:48 +0100

    Adds link to flight details on weglide.org for each flight in widget

--- js/flights.js
+++ js/flights.js
@@ -1,22 +1,28 @@
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
     var baseUrl = window.$pagekit.url;
     var flightsUrl = baseUrl + '/weglide/flights';
 
     var xmlHTTP = new XMLHttpRequest();
-    	    
+
     xmlHTTP.open('GET', flightsUrl, true);
     xmlHTTP.responseType = 'json';
-    
-    xmlHTTP.onload = function(e) {
+
+    xmlHTTP.onload = function (e) {
         var flightsData = this.response;
 
         flightsData.forEach(flight => {
+            var flightId = flight.id;
             var userName = flight.user.name;
             var distance = Math.round(flight.contest.distance);
 
-            $('#weglide-flights tbody').append(`<tr><td>${distance} km</td><td>${userName}</td></tr>`);
+            $('#weglide-flights tbody')
+                .append(`<tr>
+                    <td>${distance} km</td>
+                    <td>${userName}</td>
+                    <td><a href="https://beta.weglide.org/flights/${flightId}" target="_blank" class="uk-icon-plane"></a></td>
+                </tr>`);
         });
     };
-    
+
     xmlHTTP.send();
 });
